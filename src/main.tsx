35c0d01e10ae54d080d00fcef17c63b7e6c874f6
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-02-16 22:04:36 +0100

    Add React Query Devtools to App

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 // Initializing a new QueryClient instance
 // QueryClient manages queries and mutations, including caching, background updates, and stale data handling
 const queryClient = new QueryClient();
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     {/* QueryClientProvider wraps the App component, providing the QueryClient context to the entire React component tree */}
     {/* This allows any component within App to use TanStack Query's hooks like `useQuery` and `useMutation` */}
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
